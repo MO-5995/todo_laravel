34b37453a1ab5894e3525cd4e6392e71c7155f59
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-10-11 21:33:04 +0900

    add:controller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('/todos',App\Http\Controllers\TodoController::class);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
